b5d6e876eda314ae91142e89b66f3306e74fa494
Maxim Gostry <rotoff7@mail.ru>
2023-01-15 15:43:29 +0300

    Task#02

---
+++ Task#02/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Решение без ввода дополнительных переменных:
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(number1);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(number2);
+}
+else if (number1 < number2)
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(number2);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(number1);
+}
+else
+    Console.WriteLine("Введенные числа равны");
+
+
+// Через дополнительные переменные:
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+// int min = 0;
+// if (number1 > number2)
+// {
+//     max = number1;
+//     min = number2;
+//     Console.Write("Большее число: ");
+//     Console.WriteLine(max);
+//     Console.Write("Меньшее число: ");
+//     Console.WriteLine(min);
+// }
+// else if (number1 < number2)
+// {
+//     max = number2;
+//     min = number1;
+//     Console.Write("Большее число: ");
+//     Console.WriteLine(max);
+//     Console.Write("Меньшее число: ");
+//     Console.WriteLine(min);
+// }
+// else
+//     Console.WriteLine("Введенные числа равны");
--- Task#03/Program.cs
+++ Task#03/Program.cs
@@ -21,3 +21,36 @@ else if (number == 7)
     Console.WriteLine("Соответствующий номеру день - Воскресенье");
 else
     Console.WriteLine("Введено некорректное значение");
+
+
+// Решение через SwitchCase
+
+//     Console.Write("Введите номер: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// switch (number)
+// {
+//     case 1:
+//         Console.WriteLine("Соответствующий номеру день - Понедельник");
+//         break;
+//     case 2:
+//         Console.WriteLine("Соответствующий номеру день - Вторник");
+//         break;
+//     case 3:
+//         Console.WriteLine("Соответствующий номеру день - Среда");
+//         break;
+//     case 4:
+//         Console.WriteLine("Соответствующий номеру день - Четверг");
+//         break;
+//     case 5:
+//         Console.WriteLine("Соответствующий номеру день - Пятница");
+//         break;
+//     case 6:
+//         Console.WriteLine("Соответствующий номеру день - Суббота");
+//         break;
+//     case 7:
+//         Console.WriteLine("Соответствующий номеру день - Воскресенье");
+//         break;
+//     default:
+//         Console.WriteLine("Введено некорректное значение");
+//         break;
+// }
