00e851a5a4ea3735b7a1776c0d88343eb7146b8d
Maxim Gostry <rotoff7@mail.ru>
2023-01-16 17:33:12 +0300

    Upadte for Task#07

--- Task#07/Program.cs
+++ Task#07/Program.cs
@@ -5,5 +5,10 @@
 
 Console.Write("Введите любое трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number>=100 & number <=999 || number>=-999 & number <=-100)
+{
 Console.Write("Последняя цифра данного числа - ");
 Console.WriteLine(number % 10);
+}
+else
+Console.Write("Введено некорректное число");
