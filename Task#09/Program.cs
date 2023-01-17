cf2673fb7cc1ca90a1068c9ec769b621e1663644
Maxim Gostry <rotoff7@mail.ru>
2023-01-17 20:17:54 +0300

    Task#09

---
+++ Task#09/Program.cs
@@ -0,0 +1,42 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// int RandomNum = new Random().Next(10, 100);
+// Console.WriteLine("Случайное число: " + RandomNum);
+// int FirstDigit = RandomNum / 10;
+// int SecondDigit = RandomNum % 10;
+// if (FirstDigit > SecondDigit)
+//     Console.Write("Наибольшая цифра случайного числа -> " + FirstDigit);
+// else
+//     Console.Write("Наибольшая цифра случайного числа -> " + SecondDigit);
+
+// int result = FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
+// Console.Write("Наибольшая цифра случайного числа -> " + result);
+
+int MaxDigit(int num)
+{
+    int FirstDigit = num / 10;
+    int SecondDigit = num % 10;
+    int result = FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
+    return (result);
+}
+
+int RandomNum1 = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " + RandomNum1);
+Console.WriteLine("Наибольшая цифра случайного числа -> " + MaxDigit(RandomNum1));
+
+int RandomNum2 = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " + RandomNum2);
+Console.WriteLine("Наибольшая цифра случайного числа -> " + MaxDigit(RandomNum2));
+
+int RandomNum3 = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " + RandomNum3);
+Console.WriteLine("Наибольшая цифра случайного числа -> " + MaxDigit(RandomNum3));
+
+int RandomNum4 = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " + RandomNum4);
+Console.WriteLine("Наибольшая цифра случайного числа -> " + MaxDigit(RandomNum4));
