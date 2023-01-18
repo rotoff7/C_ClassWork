849014d384f194a42e6ec95348aff7ca7e284cd5
Maxim Gostry <rotoff7@mail.ru>
2023-01-18 14:10:23 +0300

    Task#12_v2.0

--- Task#12/Program.cs
+++ Task#12/Program.cs
@@ -23,13 +23,36 @@
 
 // Альтернативный вариант без войд функции и с возвратом переменной.
 
+// int Multiplicity(int arg1, int arg2)
+// {
+//     int result; // Объявляю переменную для дальнейшей записи результата функции
+//     if (arg1 % arg2 == 0)
+//         result = 0;
+//     else
+//         result = arg1 % arg2;
+//     return (result);
+// }
+
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int globalResult = Multiplicity(num1, num2);
+
+// if (globalResult == 0)
+// Console.WriteLine("Число " + num1 + " кратно " + num2);
+// else
+// Console.WriteLine("Число " + num1 + " не кратно " + num2 + ", остаток = " + globalResult);
+
+
+
+// Без условия в функции (В семинаре - основной).
+
 int Multiplicity(int arg1, int arg2)
 {
-    int result; // Объявляю переменную для дальнейшей записи результата функции
-    if (arg1 % arg2 == 0)
-        result = 0;
-    else
-        result = arg1 % arg2;
+    int result = arg1 % arg2;
     return (result);
 }
 
@@ -38,9 +61,9 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-Multiplicity(num1, num2); //Тут должны из функции получить переменную result.
+int globalResult = Multiplicity(num1, num2);
 
-if (result == 0) // Почему-то считает, что такой переменной не существует
-Console.WriteLine("Число " + num1 + " кратно " + num2);
+if (globalResult == 0) // Почему-то считает, что такой переменной не существует
+    Console.WriteLine("Число " + num1 + " кратно " + num2);
 else
-Console.WriteLine("Число " + num1 + " не кратно " + num2 + ", остаток = " + result);
+    Console.WriteLine("Число " + num1 + " не кратно " + num2 + ", остаток = " + globalResult);
