80c8733df048e70ac33bcda1ecd6a827613423ab
Maxim Gostry <rotoff7@mail.ru>
2023-01-31 21:35:35 +0300

    Task#40, Task#42

---
+++ Task#40/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+
+
+bool TriangleCheck(int a, int b, int c)
+{
+   return a < (b + c) && b < (a + c) && c < (a + b);
+}
+
+// Console.WriteLine("Введите три стороны треугольника.");
+// Console.Write("Введите сторону a: ");
+// int a;
+// if (int.TryParse(Console.ReadLine(), out a))
+// {
+//     Console.Write("Введите сторону b: ");
+//     int b;
+//     if (int.TryParse(Console.ReadLine(), out b))
+//     {
+//         Console.Write("Введите сторону c: ");
+//         int c;
+//         if (int.TryParse(Console.ReadLine(), out c))
+//         {
+//             bool res = TriangleCheck(a, b, c);
+//             Console.WriteLine(res ? "Да" : "Нет");
+//         }
+//         else Console.WriteLine("Введено некорректное значение.");
+//     }
+//     else Console.WriteLine("Введено некорректное значение.");
+// }
+// else Console.WriteLine("Введено некорректное значение.");
+
+Console.WriteLine("Введите три стороны треугольника.");
+Console.Write("Введите сторону a: ");
+int sideA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону b: ");
+int sideB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону c: ");
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+bool res = TriangleCheck(sideA, sideB, sideC);
+Console.WriteLine(res ? "Да" : "Нет");
---
+++ Task#42/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+//  300. 15 1. 7 1. 3 1. 1           = 01111   /    11110
+
+int NumBinary(int number)
+{
+    int numberBin = 0;
+    int count = 1;
+    while (number > 0)
+    {
+        numberBin = numberBin + (number % 2 * count);
+        number /= 2;
+        count *= 10;
+    }
+    return numberBin;
+}
+
+Console.Write("Введите число: ");
+int num;
+if (int.TryParse(Console.ReadLine(), out num))
+{
+    if (num >= 0)
+    {
+    int result = NumBinary(num);
+    Console.WriteLine(result);
+    }
+    else Console.WriteLine("Введено отрицательное число.");
+}
+else Console.WriteLine("Введено некорректное значение.");
