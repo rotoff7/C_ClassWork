4ac95b399828db8811772c77dcdec785075f7938
Maxim Gostry <rotoff7@mail.ru>
2023-02-10 16:08:55 +0300

    Task#60_Complete!

--- Task#60/Program.cs
+++ Task#60/Program.cs
@@ -5,6 +5,11 @@
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
 
+
+// Скорее всего данное решение страдает в плане оптимизации.
+// Но задача по созданию случайных неповторяющихся чисел поставил в сильное затруднение.
+// И без использования гугла дошел вот до такого решения.
+
 void IncorrectValue()
 {
     Console.WriteLine("Введено некорректное значение");
@@ -17,21 +22,51 @@ int UserInput()
     return temp;
 }
 
-int[,,] Matrix3DCreate(int rows, int columns, int depth, int min, int max)
+int[,,] Matrix3DCreate(int rows, int columns, int depth) // создаем пустой 3-x массив
 {
     int[,,] array3D = new int[rows, columns, depth];
+    return array3D;
+}
+
+int RandomValue(int min, int max)
+{
     Random rnd = new Random();
-    for (int i = 0; i < rows; i++)
+    int value = rnd.Next(min, max + 1);
+    return value;
+}
+
+bool UniqueValueCheck(int[,,] array3D, int value) // Проходим по всему массиву и проверяем совпадения
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
     {
-        for (int j = 0; j < columns; j++)
+        for (int j = 0; j < array3D.GetLength(1); j++)
         {
-            for (int k = 0; k < depth; k++)
+            for (int k = 0; k < array3D.GetLength(2); k++)
             {
-                array3D[i, j, k] = rnd.Next(min, max + 1);
+                if (array3D[i, j, k] == value) return true;
             }
         }
     }
-    return array3D;
+    return false;
+}
+
+void Matrix3DFiller(int[,,] array3D, int min, int max) // Заполняем 3-x массив уникальными двузначными числами
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                int value = RandomValue(min, max);
+                while (UniqueValueCheck(array3D, value) == true)
+                {
+                    value = RandomValue(min, max);
+                }
+                array3D[i, j, k] = value;
+            }
+        }
+    }
 }
 
 void Print3DMatrix(int[,,] array3D)
@@ -64,6 +99,12 @@ if (depthSize <= 0) IncorrectValue();
 int minValue = 10;
 int maxValue = 99;
 
-int[,,] matrix = Matrix3DCreate(rowsSize, columnsSize, depthSize, minValue, maxValue);
-Console.WriteLine("Заданный массив:");
-Print3DMatrix(matrix);
+int[,,] matrix = Matrix3DCreate(rowsSize, columnsSize, depthSize);
+if (matrix.Length <= 90)
+{
+    Matrix3DFiller(matrix, minValue, maxValue);
+    Console.WriteLine("Заданный массив:");
+    Print3DMatrix(matrix);
+}
+else Console.WriteLine("Кол-во элементов в массиве с такими параметрами превышает 90. "
++ "Невозможно заполнить данный массив неповторяющимися двузначными числами.");
